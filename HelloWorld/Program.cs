d6f65a1bdf8f5d0fe28b603f38eb634afc36a087
pablitohaddad <pablohaddad73@gmail.com>
2024-09-14 10:00:35 -0400

    Primeiros passos na documentação oficial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Whats is your name?");
+        var name = Console.ReadLine();
+        var currentData = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentData:d} at {currentData:t}!"); // d = day, t = time
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true); // aguarda a entrada do usuário.
+    }
+}
